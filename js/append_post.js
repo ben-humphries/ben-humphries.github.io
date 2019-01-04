1b884d60ad2c652e2ddce908534c26d9c90edcd2
Ben Humphries <benfhumphries@gmail.com>
2019-01-04 14:44:22 -0500

    Fixed post loading, now loads in the correct order

--- js/append_post.js
+++ js/append_post.js
@@ -1,7 +1,8 @@
 function append_post(url){
+
  	var rawFile = new XMLHttpRequest();
- 	console.log("here");
     rawFile.open("GET", url);
+
     rawFile.onload = function () {
         if(rawFile.readyState === 4)
         {
@@ -11,21 +12,24 @@ function append_post(url){
                 div = document.createElement('div');
                 div.innerHTML = allText;
                 document.getElementById(url).appendChild(div);
-                //dummy commit
             }
         }
     }
-    rawFile.send(null);
-  }
 
-  function append_all_posts(urls){
-    urls_list = urls.split(',');
-    console.log(urls_list);
-    for(i = 0; i < urls_list.length; i++){
-        div = document.createElement('div');
-        div.setAttribute("id", urls_list[i]);
-    }
-    for(i = 0; i < urls_list.length; i++){
-        append_post(urls_list[i]);
-    }
-  }
+    rawFile.send(null);
+}
+
+function append_all_posts(urls){
+
+urls_list = urls.split(',');
+
+for(i = 0; i < urls_list.length; i++){
+    div = document.createElement('div');
+    div.setAttribute("id", urls_list[i]);
+    document.getElementById("posts").appendChild(div);
+}
+
+for(i = 0; i < urls_list.length; i++){
+    append_post(urls_list[i]);
+}
+}
