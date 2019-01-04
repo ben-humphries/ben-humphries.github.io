3fb1875783c513757f3cee924892bbaa860e3b58
Ben Humphries <benfhumphries@gmail.com>
2019-01-04 14:28:50 -0500

    separate div for each post

--- js/append_post.js
+++ js/append_post.js
@@ -2,9 +2,6 @@ function append_post(url){
  	var rawFile = new XMLHttpRequest();
  	console.log("here");
     rawFile.open("GET", url);
-    rawFile.onreadystatechange = function ()
-    {
-    }
     rawFile.onload = function () {
         if(rawFile.readyState === 4)
         {
@@ -13,7 +10,7 @@ function append_post(url){
                 var allText = rawFile.responseText;
                 div = document.createElement('div');
                 div.innerHTML = allText;
-                document.getElementById("posts").appendChild(div);
+                document.getElementById(urls_list[i]).appendChild(div);
             }
         }
     }
@@ -23,6 +20,10 @@ function append_post(url){
   function append_all_posts(urls){
     urls_list = urls.split(',');
     console.log(urls_list);
+    for(i = 0; i < urls_list.length; i++){
+        div = document.createElement('div');
+        div.setAttribute("id", urls_list[i]);
+    }
     for(i = 0; i < urls_list.length; i++){
         append_post(urls_list[i]);
     }
